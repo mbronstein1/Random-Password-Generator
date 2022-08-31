40310c0c603eb11890b750da39cb4db390f65181
Matthew Bronstein <mbronstein1@gmail.com>
2022-08-31 17:05:26 -0500

    Finished script.js file and completed acceptance criteria for project

--- assets/script.js
+++ assets/script.js
@@ -1,101 +1,110 @@
-// Assignment Code
+// My Pseudocode steps for solution code:
+    //(DONE)Create generatePassword() function and place all code inside;
+    //(DONE) Create the following popups for the user to interact with:
+      //(DONE) Prompt asking for number of characters w/ a min and max value w/ entered value as the argument for the length parameter;
+        //(DONE) If input is not within parameters or is not a number, alert and try again;
+      //(DONE) Confirm whether lowercase characters are allowed then either continue or remove them as options;
+      //(DONE) Confirm whether uppercase characters are allowed then either continue or remove them as options;
+      //(DONE) Confirm whether numeric characters are allowed then either continue or remove them as options;
+      //(DONE) Confirm whether special characters are allowed then either continue or remove them as options;
+       //(DONE) If none were selected, alert w/ a statement that at least one of the above must be selected and begin again;
+    //(DONE) Concatenate all available strings from above prompts into giant string and assign it as a new variable;
+    //(DONE) Convert giant string to giant array using .split("") and assign it as a new variable;
+    //(DONE) Randomize and sort giant array and assign it as a new variable;
+    //(DONE) Slice giant random array starting from 0 and moving to the value of numChar and assign a new variable;
+    //(DONE) Convert sliced array back to string without commas with .join("") and assign a new variable;
+    //(DONE) Return final variable!!
+
 var generateBtn = document.querySelector("#generate");
 
+//Create generatePassword() function
+function generatePassword() {
+  var numOfCharacters;
+  var upperCase;
+  var lowerCase;
+  var number;
+  var special;
+  //Create prompt asking for number of characters and alert if input is invalid  
+  var numOfCharactersPrompt = function(input) {
+    var input = Number(prompt("How many characters would you like? The number must be between 8 and 128."));
+    while ((input < 8) || (input > 128) || (isNaN(input))) { 
+        alert("Invalid input. Please try again.")
+        var input = prompt("How many characters would you like? The number must be between 8 and 128.");
+    }
+    numOfCharacters = input;
+    return numOfCharacters;
+  };
 
-// Prompt popup asking for numbers of characters w/ min/max value -- stored in numChar variable
-var numChar;
-var numCharPrompt = function(input) {
-  input = Number(prompt("How many characters would you like? The number must be between 8 and 128."));
-  while ((input < 8) || (input > 128) || (isNaN(input))) { 
-      alert("Invalid input. Please try again.")
-      var input = prompt("How many characters would you like? The number must be between 8 and 128.");
-  }
-  numChar = input;
-  return numChar;
+  // Confirming characters and reassigning variables as strings or leaving variables as empty strings if not selected in popup box
+    //Created long-enough strings where if user only selects ONE of these character types that the password can still reach the maximum value of 128
+  var upperChar = function() {
+    if (confirm("Would you like to include uppercase characters?")) {
+      upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    } else {
+      upperCase = "";
+    }
+    return upperCase;
+  };
+
+  var lowerChar = function() {
+    if (confirm("Would you like to include lowercase characters?")) {
+      lowerCase = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
+    } else {
+      lowerCase = "";
+    }
+    return lowerCase;
+  };
+
+  var numChar = function() {
+    if(confirm("Would you like to include numeric characters?")) {
+      number = "0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789";
+    } else {
+      number = "";
+    }
+    return number;
+  };
+
+  var specChar = function() {
+    if(confirm("Would you like to include special characters?")) {
+      special = `!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?`;
+    } else {
+      special = "";
+    }
+    return special;
+  };
+
+  // Validating that at least one confirm popup was selected, otherwise confirm popups start again
+  function checkChar() {
+    upperChar();
+    lowerChar();
+    numChar();
+    specChar();
+    if (upperCase === "" && lowerCase === "" && number === "" && special === "") {
+      alert("At least one character type must be selected.");
+      checkChar();
+    }
+  };
+
+  //Execute functions
+  numOfCharactersPrompt();
+  checkChar();
+
+  // Concatenate all character strings and turn them into an array
+  var concatString = upperCase + lowerCase + number + special;
+  var stringToArray = concatString.split("");
+
+  //-------------
+  //Taken from StackOverflow to iterate through the array then randomize it
+  //Citation: superluminary.(2017, Oct 3). "How to randomize (shuffle) a JavaScript array?". StackOverflow. https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  var randomArray = stringToArray.map(value => ({ value, sort: Math.random() })).sort((a, b) => a.sort - b.sort).map(({ value }) => value);
+
+  // Create new array that contains just the number of characters inputted by user at beginning of function
+  var randomArraySliced = randomArray.slice(0, numOfCharacters);
+
+  //Return Array to String with .join("")
+  var newPassword = randomArraySliced.join("");
+  return newPassword;
 };
-numCharPrompt();
-
-//Confirm popups confirming whether to use uppercase, lowercase, numeric, and special characters
-//--------------------------
-// OPTION 1 - Confirming characters and converting to array w/ commas -- stored in array1, array2, array3, and array4 respectively
-// var array1 = [];
-// var lowerChar = function() {
-//   var lowerCase = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
-//   if (confirm("Would you like to use lowercase characters?")) {
-//     array1 = array1.concat(lowerCase.split(""));
-//   }
-//   return array1;
-// };
-// lowerChar();
-
-// var array2 = [];
-// var upperChar = function() {
-//   var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//   if (confirm("Would you like to use lowercase characters?")) {
-//     array2 = array2.concat(upperCase.split(""));
-//   }
-//   return array2;
-// };
-// upperChar();
-
-// var array3 = [];
-// var numChar = function() {
-//   var numbers = "0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789";
-//   if(confirm("Would you like to use number characters?")) {
-//     array3 = array3.concat(numbers.split(""));
-//   }
-//   return array3;
-// };
-// numChar();
-
-// var array4 = [];
-// var specChar = function() {
-//   var special = "!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?"
-//   if(confirm("Would you like to use special characters?")) {
-//     array4 = array4.concat(special.split(""));
-//   }
-//   return array4;
-// };
-// specChar();
-//----------------------------------------------
-
-// OPTION 2 (PREFERRED) - Confirming characters and leaving as strings for now (will need to concat w/ "+", convert to giant array w/ split(""), randomize and sort, and convert back to string w/ .join("")) -- stored in upperCase, lowerCase, number, and special respectively.
-var upperCase;
-var upperChar = function() {
-  if (confirm("Would you like to use lowercase characters?")) {
-    upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  }
-  return upperCase;
-};
-upperChar();
-
-var lowerCase;
-var lowerChar = function() {
-  if (confirm("Would you like to use lowercase characters?")) {
-    lowerCase = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
-  }
-  return lowerCase;
-};
-lowerChar();
-
-var number;
-var numChar = function() {
-  if(confirm("Would you like to use number characters?")) {
-    number = "0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789";
-  }
-  return number;
-};
-numChar();
- 
-var special;
-var specChar = function() {
-  if(confirm("Would you like to use special characters?")) {
-    special = `!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?!@#$%^&*?`;
-  }
-  return special;
-};
-specChar();
-
 
 // Write password to the #password input
 function writePassword() {
@@ -103,26 +112,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
-// generatePassword function
-function generatePassword() {
-}
-
-// Create generatePassword() function
-//(DONE) Create variables for the following:
-  //(DONE) Prompt asking for number of characters w/ a min and max value w/ entered value as the argument for the length parameter
-  //(DONE) Confirming whether lowercase characters are allowed then either move on or remove them as options
-  // (DONE) Confirming whether uppercase characters are allowed then either move on or remove them as options
-  // (DONE) Confirming whether numeric characters are allowed then either move on or remove them as options
-  // (DONE) Confirming whether special characters are allowed then either move on or remove them as options
-  // Alert w/ at least one of the above must be selected
-// Concat all available strings into giant string and assign it as a new variable
-// Convert giant string to giant array using .split("");
-// Randomize and sort giant array;
-//.splice() or .slice() starting from 0 and moving to the value of numChar;
-// When finished, return array to string without commas with array.join("");
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
